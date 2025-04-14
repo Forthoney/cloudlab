b67edbb75bf42d0e8041e483ca71e88a002e46db
Forthoney <castlehoneyjung@gmail.com>
2025-04-14 14:46:52 -0400

    pash benchmark profile

---
+++ profile.py
@@ -0,0 +1,10 @@
+import geni.portal as portal
+import geni.rspec.pg as pg
+
+pc = portal.Context()
+request = pc.makeRequestRSpec()
+
+node = request.RawPC("node")
+node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
+
+pc.printRequestRSpec(request)
---
+++ setup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/binpash/benchmarks.git
+git clone --branch caruca-eval --single-branch https://github.com/binpash/pash.git
+sudo pash/scripts/distro-deps.sh
+yes | sudo pash/scripts/setup-pash.sh
