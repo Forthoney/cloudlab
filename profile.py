feed3afa4d4b938d12dc76ed15aaff12d3029c9f
Forthoney <castlehoneyjung@gmail.com>
2025-04-14 15:17:35 -0400

    modify setup script

--- profile.py
+++ profile.py
@@ -4,8 +4,30 @@ import geni.rspec.pg as pg
 pc = portal.Context()
 request = pc.makeRequestRSpec()
 
+pc.defineParameter(
+    "phystype", "Optional physical node type", portal.ParameterType.NODETYPE, ""
+)
+
+params = pc.bindParameters()
+
+if params.phystype != "":
+    if len(params.phystype.split(",")) != 1:
+        pc.reportError(
+            portal.ParameterError("Only a single type is allowed", ["phystype"])
+        )
+
+pc.verifyParameters()
+
 node = request.RawPC("node")
-node.hardware_type = "r320"
-node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
+if params.phystype != "":
+    node.hardware_type = params.phystype
+node.addService(
+    pg.Execute(shell="sh", command="sudo /local/repository/pash/scripts/distro-deps.sh")
+)
+node.addService(
+    pg.Execute(
+        shell="sh", command="yes | sudo /local/repository/pash/scripts/setup-pash.sh"
+    )
+)
 
 pc.printRequestRSpec(request)
--- setup.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/binpash/benchmarks.git
-git clone --branch caruca-eval --single-branch https://github.com/binpash/pash.git
-sudo pash/scripts/distro-deps.sh
-yes | sudo pash/scripts/setup-pash.sh
